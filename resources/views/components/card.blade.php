dd1e2fad45629ce8ad931c5dd5ef7974eebba07f
milky_way <shin.kagami13@gmail.com>
2024-04-27 10:52:24 +0700

    feat(color) : change bg color card datatable

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 <!-- card.blade.php -->
 <div>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
         <div class="px-4 py-4 mx-2">
             <h3 class="font-bold text-xl mb-2">{{ $title }}</h3>
             <div class="mt-4">
