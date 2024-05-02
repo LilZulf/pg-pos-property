aea27f70ef79d234cd30ce3546c77cc9c73a7516
anipitriyaz <anirizki009@gmail.com>
2024-05-02 22:24:23 +0700

    create-transaction page /revision

--- resources/views/transactions/create-transaction.blade.php
+++ resources/views/transactions/create-transaction.blade.php
@@ -25,6 +25,9 @@
 <div class="row">
     <div class="col-xxl-12">
         <div class="card">
+        <div class="card-header align-items-center d-flex">
+                                    <h4 class="card-title mb-0 flex-grow-1">Create new transaction</h4>
+                                </div>
             <div class="card-body">
                 <div class="live-preview">
                     <form action="javascript:void(0);">
